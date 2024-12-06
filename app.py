0b4d473d559b1705d0c2595768adf59dd86c5ded
Dimitri Blanchard <kor@tuta.com>
2024-12-06 22:21:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import os
+import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
 import yt_dlp
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+# Autoriser les requêtes CORS de n'importe quelle origine
+CORS(app, resources={r"/*": {"origins": "*"}})
 
+# Charger les cookies depuis les variables d'environnement
+cookie_data = os.getenv("YOUTUBE_COOKIES")
+if cookie_data:
+    cookies = json.loads(cookie_data)  # Convertir les cookies de JSON en dictionnaire
+else:
+    cookies = None
+    print("Aucun cookie trouvé dans les variables d'environnement")
 
 @app.route("/")
 def home():
@@ -21,14 +34,16 @@ def download_song():
         return jsonify({"error": "No song name provided"}), 400
 
     try:
+        # Options pour yt-dlp
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': 'static/%(title)s.%(ext)s',
             'quiet': True,
-            'cookiesfrombrowser': ('chrome',),  # Remplacez 'chrome' par 'firefox' ou 'edge' selon votre navigateur
+            'cookies': cookies  # Utiliser les cookies récupérés depuis les variables d'environnement
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            # Recherche et téléchargement de la chanson
             info_dict = ydl.extract_info(f"ytsearch:{song_name}", download=True)
             file_name = f"{info_dict['entries'][0]['title']}.{info_dict['entries'][0]['ext']}"
 
@@ -38,6 +53,8 @@ def download_song():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == "__main__":
+    # Créer le dossier static si il n'existe pas
     if not os.path.exists('static'):
         os.makedirs('static')
+    # Exécuter l'application
     app.run(debug=False, host="0.0.0.0", port=5000)
